248992d502bf6a4972b08d689ac5806cf03409ba
Your Name <you@example.com>
2026-07-06 23:32:10 +0500

    fix: add missing import for ModalManager in i18n.ts ```

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,7 @@
 import i18n from 'i18next';
 
+import { ModalManager } from './modalManager.js';
+
 import enTranslations from './locales/en.json';
 import ruTranslations from './locales/ru.json';
 
